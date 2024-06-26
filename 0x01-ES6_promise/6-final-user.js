9ef288ad303b57b18102e50e732f4f8697c1ef91
abeni-al7 <abenezeralebachew3@gmail.com>
2024-06-27 00:41:21 +0300

    Avoid async in the solution for 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
