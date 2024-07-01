b5651a77589c3119010b581dd82893557aacdddb
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-02 00:44:49 +0300

    Handle error

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    const count = weakMap.get(endpoint);
+    let count = weakMap.get(endpoint);
+    count += 1;
     if (count >= 5) {
       throw new Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, count + 1);
+    weakMap.set(endpoint, count);
   } else {
     weakMap.set(endpoint, 1);
   }
