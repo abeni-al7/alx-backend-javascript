6ceef5656c900251c148b40ff93e48875cb5d737
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-23 22:43:24 +0300

    Integration test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,10 +2,16 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('Index Page', () => {
-  it('should return the correct message and status code', () => {
+  it('should return the correct status code', () => {
     const url = 'http://localhost:7865';
     request(url, (error, response, body) => {
       expect(response.statusCode).to.equal(200);
+    });
+  });
+
+  it('should return the correct message', () => {
+    const url = 'http://localhost:7865';
+    request(url, (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
     });
   });
